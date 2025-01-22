83c88cf985bea697c5cbd5fefb44ad302dc551d4
Gabriel Hinojoza <omar.hinojoza13@gmail.com>
2025-01-21 23:31:53 -0600

    Remove MySQL service from docker-compose and update hero component schema in Strapi. Added buttons and description attributes to hero component, and introduced shared button component schema. Also, added nav-bar schema with buttons attribute for better navigation management.

--- strapi/types/generated/components.d.ts
+++ strapi/types/generated/components.d.ts
@@ -7,8 +7,21 @@ export interface HomeHero extends Struct.ComponentSchema {
     displayName: 'hero';
   };
   attributes: {
+    buttons: Schema.Attribute.Component<'shared.button', true>;
+    description: Schema.Attribute.Text & Schema.Attribute.Required;
     media: Schema.Attribute.Media<'images' | 'files' | 'videos'>;
     title: Schema.Attribute.String & Schema.Attribute.Required;
+  };
+}
+
+export interface SharedButton extends Struct.ComponentSchema {
+  collectionName: 'components_shared_buttons';
+  info: {
+    displayName: 'Button';
+  };
+  attributes: {
+    className: Schema.Attribute.Text;
+    content: Schema.Attribute.String;
   };
 }
 
@@ -27,6 +40,7 @@ declare module '@strapi/strapi' {
   export module Public {
     export interface ComponentSchemas {
       'home.hero': HomeHero;
+      'shared.button': SharedButton;
       'text.paragraph': TextParagraph;
     }
   }
--- strapi/types/generated/contentTypes.d.ts
+++ strapi/types/generated/contentTypes.d.ts
@@ -369,6 +369,34 @@ export interface AdminUser extends Struct.CollectionTypeSchema {
   };
 }
 
+export interface ApiNavBarNavBar extends Struct.SingleTypeSchema {
+  collectionName: 'nav_bars';
+  info: {
+    displayName: 'nav-bar';
+    pluralName: 'nav-bars';
+    singularName: 'nav-bar';
+  };
+  options: {
+    draftAndPublish: true;
+  };
+  attributes: {
+    buttons: Schema.Attribute.Component<'shared.button', true>;
+    createdAt: Schema.Attribute.DateTime;
+    createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
+      Schema.Attribute.Private;
+    locale: Schema.Attribute.String & Schema.Attribute.Private;
+    localizations: Schema.Attribute.Relation<
+      'oneToMany',
+      'api::nav-bar.nav-bar'
+    > &
+      Schema.Attribute.Private;
+    publishedAt: Schema.Attribute.DateTime;
+    updatedAt: Schema.Attribute.DateTime;
+    updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
+      Schema.Attribute.Private;
+  };
+}
+
 export interface ApiPagePage extends Struct.CollectionTypeSchema {
   collectionName: 'pages';
   info: {
@@ -906,6 +934,7 @@ declare module '@strapi/strapi' {
       'admin::transfer-token': AdminTransferToken;
       'admin::transfer-token-permission': AdminTransferTokenPermission;
       'admin::user': AdminUser;
+      'api::nav-bar.nav-bar': ApiNavBarNavBar;
       'api::page.page': ApiPagePage;
       'plugin::content-releases.release': PluginContentReleasesRelease;
       'plugin::content-releases.release-action': PluginContentReleasesReleaseAction;
