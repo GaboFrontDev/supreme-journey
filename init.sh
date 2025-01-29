2ac13f3f98b271953e57d7b6f040e26bb5becc8f
Gabriel Hinojoza <omar.hinojoza13@gmail.com>
2025-01-29 03:06:16 -0600

    Enhance init.sh script with improved runtime and serve functionality
    - Add dynamic runtime detection for bun/npm
    - Introduce serve command to start Next.js output
    - Support flexible parameter handling for Next.js commands
    - Add fallback for second parameter to "dev"

--- init.sh
+++ init.sh
@@ -1,11 +1,21 @@
 PARAM1=$1
-PARAM2=$2
+# pick the second parameter or set it to be "dev"
+PARAM2=$2 || "dev"
 # check if bun is installed else use npm
 if ! command -v bun &> /dev/null; then
     echo "Bun is not installed, using npm instead"
     runtime="npm"
+    runner="npx"
 else
     runtime="bun"
+    runner="bunx"
+fi
+
+if [ "$PARAM1" == "serve" ]; then
+    echo "Starting server..."
+    cd next
+    $runner serve out
+    exit 0
 fi
 
 if [ "$PARAM1" == "next" ]; then
@@ -18,7 +28,7 @@ if [ "$PARAM1" == "next" ]; then
     if [ "$PARAM2" == "build" ]; then
         $runtime run build
     else
-        $runtime run dev
+        $runtime run $PARAM2
     fi
 
 
