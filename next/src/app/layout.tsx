3a4dfa328bf14104d50e25d358907bf77ee3b51f
Gabriel Hinojoza <omar.hinojoza13@gmail.com>
2025-06-01 18:03:57 -0600

    chore: update site metadata with new title and description for Ares Arquitectos

--- next/src/app/layout.tsx
+++ next/src/app/layout.tsx
@@ -2,8 +2,8 @@ import '@/styles/globals.css';
 import { ReactNode } from 'react';
 
 export const metadata = {
-  title: 'Aura Imaging',
-  description: 'Sitio en construcción',
+  title: 'Ares Arquitectos',
+  description: 'Basada en Guadalajara, Jalisco, México, la empresa ha diseñado los más exitosos proyectos comerciales del país, creciendo de la mano de los desarrolladores inmobiliarios que los han hecho realidad, lo que nos ha llevado a posicionarnos como líder de la industria.',
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
