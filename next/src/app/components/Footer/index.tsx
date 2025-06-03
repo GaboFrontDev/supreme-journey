a28bf701e53ca092bf4159a756d45dff33dd73a6
Gabriel Hinojoza <omar.hinojoza13@gmail.com>
2025-06-03 17:01:25 -0600

    send mail

--- next/src/app/components/Footer/index.tsx
+++ next/src/app/components/Footer/index.tsx
@@ -23,7 +23,9 @@ export default function Footer({ offices }: FooterProps) {
                 <p className='text-xs font-bold'>{office.name}</p>
                 <ul className='space-y-1 text-xs leading-5 text-c-gray-200'>
                   <li>{office.phone}</li>
-                  <li>{office.email}</li>
+                  <li>
+                    <a href={`mailto:${office.email}`}>{office.email}</a>
+                  </li>
                 </ul>
               </div>
             </div>
