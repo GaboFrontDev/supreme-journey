940cc7381a33125b0cf5d2a4b7f3e1feb139a800
Gabriel Hinojoza <omar.hinojoza13@gmail.com>
2025-08-08 14:34:36 -0600

    add meta head

--- next/src/app/projects/[category]/page.tsx
+++ next/src/app/projects/[category]/page.tsx
@@ -2,6 +2,7 @@ import CategoryPageComponent from './component';
 import { notFound } from 'next/navigation';
 import { getCategories } from '@/dynamicRendering/utils';
 import { formatSlug } from '@/utils/formatSlug';
+import Head from 'next/dist/shared/lib/head';
 
 export async function generateStaticParams() {
   const categories = await getCategories();
@@ -25,5 +26,57 @@ export default async function CategoryPage({
     return notFound();
   }
 
-  return <CategoryPageComponent category={category} />;
+  return (
+    <>
+      <Head>
+        <title>{category.attributes.nombre} - Proyectos ARES</title>
+        <meta name='description' content={category.attributes.concepto} />
+        <meta name='keywords' content={category.attributes.nombre} />
+        <meta
+          property='og:title'
+          content={`${category.attributes.nombre} - Proyectos ARES`}
+        />
+        <meta
+          property='og:description'
+          content={category.attributes.concepto}
+        />
+        <meta property='og:type' content='website' />
+        <meta
+          property='og:url'
+          content={`https://ares.proyectos.uchile.cl/projects/${params.category}`}
+        />
+        {category.attributes.portada.data.attributes.previewUrl && (
+          <meta
+            property='og:image'
+            content={category.attributes.portada.data.attributes.previewUrl}
+          />
+        )}
+        <meta
+          property='og:image:alt'
+          content={category.attributes.nombre}
+        />
+        <meta
+          property='og:image:width'
+          content={category.attributes.portada.data.attributes.width.toString()}
+        />
+        <meta
+          property='og:image:height'
+          content={category.attributes.portada.data.attributes.height.toString()}
+        />
+        <meta name='twitter:card' content='summary_large_image' />
+        <meta name='twitter:title' content={category.attributes.nombre} />
+        <meta
+          name='twitter:description'
+          content={category.attributes.concepto}
+        />
+        {category.attributes.portada.data.attributes.previewUrl && (
+          <meta
+            name='twitter:image'
+            content={category.attributes.portada.data.attributes.previewUrl}
+          />
+        )}
+      </Head>
+      <CategoryPageComponent category={category} />
+    </>
+  );
 }
